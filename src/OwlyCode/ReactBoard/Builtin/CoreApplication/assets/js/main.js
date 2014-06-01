be9dd4657508228b87294c74c3d8351f6fae7e6b
OwlyCode <tmaindron@gmail.com>
2014-06-01 22:09:10 +0200

    Updated js to trigger events so the app frontend can react to a switch

--- src/OwlyCode/ReactBoard/Builtin/CoreApplication/assets/js/main.js
+++ src/OwlyCode/ReactBoard/Builtin/CoreApplication/assets/js/main.js
@@ -3,7 +3,7 @@ $(function(){
     var module = $('#app').data('module');
 
     $('#app').load('/' + app + '/'+ module);
-    $('#app').addClass(app);
+    $('#app').addClass(app).trigger(app + '.' + module + '.activate');
     var ws = $.websocket("ws://" + document.domain + ":" + window.location.port + "/ws", {
         open: function() {
             console.log('Connexion established.');
@@ -13,12 +13,18 @@ $(function(){
         },
         events: {
             switch: function(msg){
+                var tokens = msg.url.split('/');
+                var app = tokens[1];
+                var module = tokens[2];
                 console.log('Attempting to switch to ' + msg.url);
                 $('#app').load(msg.url, function(response, status){
                     if(status == 'error') {
                         console.log('Failed to switch to ' + msg.url);
                     } else {
+                        $('#app').trigger($('#app').data('app') + '.' + $('#app').data('module') + '.deactivate');
                         $('#app').removeClass().addClass(msg.url.split('/')[1]);
+                        $('#app').data('app', app).data('module', module);
+                        $('#app').trigger(app + '.' + module + '.activate');
                         console.log('Switched to ' + msg.url);
                     }
                 });
